676889ba242c093716a4772fde732ce9b46afa2d
WEIQ311 <1107438082@qq.com>
2019-01-29 14:25:32 +0800

    添加nacos

--- src/main/java/com/report/ReportApplication.java
+++ src/main/java/com/report/ReportApplication.java
@@ -1,12 +1,16 @@
 package com.report;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 /**
  * @author weiQiang
  */
 @SpringBootApplication
+@NacosPropertySource(dataId = "zb.data.report", autoRefreshed = true)
+@RefreshScope
 public class ReportApplication {
 
     public static void main(String[] args) {
